41bd3989debf5f8b78caebaa872dbcfd980cad91
Andrew-Joan <pelukguling123@gmail.com>
2024-11-21 22:54:56 +0700

    feat: add models and migrations

---
+++ app/Http/Controllers/ProductDonationController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductDonation;
+use Illuminate\Http\Request;
+
+class ProductDonationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductDonation $productDonation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProductDonation $productDonation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProductDonation $productDonation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductDonation $productDonation)
+    {
+        //
+    }
+}
