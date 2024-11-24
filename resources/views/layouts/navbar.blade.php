0465d8b0b2e24a0ef7e65e6e6a8e8d1b29d38267
Andrew-Joan <pelukguling123@gmail.com>
2024-11-24 16:15:20 +0700

    add donation dropdown on navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,12 +1,21 @@
-<header class="bg-white">
+<header class="bg-white shadow-sm">
     <div class="container py-3 d-flex justify-content-between align-items-center">
         <div class="fs-4 fw-bold text-primary">tangankita</div>
-        <nav>
-            <a href="/" class="mx-4 text-secondary text-decoration-none">Home</a>
-            <a href="/about" class="mx-4 text-secondary text-decoration-none">About</a>
-            <a href="/donation" class="mx-4 text-secondary text-decoration-none">Donation</a>
-            <a href="/blog" class="mx-4 text-secondary text-decoration-none">Blog</a>
-            <a href="/contact" class="mx-4 text-secondary text-decoration-none">Contact</a>
+        <nav class="d-flex align-items-center">
+            <a href="/" class="mx-4 text-secondary">Home</a>
+            <a href="/about" class="mx-4 text-secondary">About</a>
+            <div class="dropdown mx-4">
+                <a class="nav-link dropdown-toggle text-secondary" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Donation
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item text-secondary" href="{{ route('fund-donation.index') }}">Fund Donation</a></li>
+                    <li><a class="dropdown-item text-secondary" href="#">Product Donation</a></li>
+                </ul>
+            </div>
+            <a href="/volunteer" class="mx-4 text-secondary">Volunteer</a>
+            <a href="/blog" class="mx-4 text-secondary">Blog</a>
+            <a href="/contact" class="mx-4 text-secondary">Contact</a>
         </nav>
         <div>
             <button class="btn btn-outline-primary me-4">Sign in</button>
@@ -14,3 +23,4 @@
         </div>
     </div>
 </header>
+
