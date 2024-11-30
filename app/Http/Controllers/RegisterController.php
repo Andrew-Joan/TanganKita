2dad44f58f34f3a9ee0d5e84cfc3a430818fbb47
Andrew-Joan <pelukguling123@gmail.com>
2024-11-30 21:05:33 +0700

    feat:register-be

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
 
 class RegisterController extends Controller
 {
@@ -11,8 +13,18 @@ class RegisterController extends Controller
         return view('register');
     }
 
-    public function store()
+    public function store(RegisterRequest $request)
     {
+        $validatedData = $request->validated();
+        
+        // pake create aja ga pake insert($validatedData), biar password auto-hash karena di model udah di cast hashed
+        User::create([
+            'email' => $validatedData['email'],
+            'password' => $validatedData['password'],
+            'name' => $validatedData['name'],
+            'date_of_birth' => $validatedData['date_of_birth'],
+        ]);
 
+        return to_route('login')->with('success', 'Registrasi berhasil');
     }
 }
