b18ca9a33ced1b36efdc94301590695ffaf9db6b
Andrew-Joan <pelukguling123@gmail.com>
2024-12-07 17:48:33 +0700

    change route

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\ContactController;
-use App\Http\Controllers\FundDonationController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\VolunteerController;
+use App\Http\Controllers\FundDonationController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
